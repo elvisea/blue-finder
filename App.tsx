6e19c979394a19994305ac415403e4e947a5c24f
Elvis <eleram@protonmail.com>
2024-03-12 19:40:29 -0300

    Third Commit 😄

--- App.tsx
+++ App.tsx
@@ -7,30 +7,16 @@ import { ScanScreen } from "./src/screens/ScanScreen";
 
 import {
   useFonts,
-  RobotoSlab_100Thin,
-  RobotoSlab_200ExtraLight,
-  RobotoSlab_300Light,
   RobotoSlab_400Regular,
   RobotoSlab_500Medium,
-  RobotoSlab_600SemiBold,
-  RobotoSlab_700Bold,
-  RobotoSlab_800ExtraBold,
-  RobotoSlab_900Black,
 } from "@expo-google-fonts/roboto-slab";
 
 import { Loading } from "./src/components/Loading";
 
 export default function App() {
   let [fontsLoaded] = useFonts({
-    RobotoSlab_100Thin,
-    RobotoSlab_200ExtraLight,
-    RobotoSlab_300Light,
     RobotoSlab_400Regular,
     RobotoSlab_500Medium,
-    RobotoSlab_600SemiBold,
-    RobotoSlab_700Bold,
-    RobotoSlab_800ExtraBold,
-    RobotoSlab_900Black,
   });
 
   return (
